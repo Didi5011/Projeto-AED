df9852d176fe12864b546100411d4866a146d0eb
celineacia <146113273+celineacia@users.noreply.github.com>
2025-09-25 23:18:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+1-Lisboa, 2-Evora, 3-Beja, 4-Faro, 5-Santarem, 6-Guarda, 7-Madrid, 8-Aveiro, 9-Porto, 10-Braga,
+11-Chaves, 12-Dili
+*/
+
+#define NONE 0
+#define LISBOA 1
+#define EVORA 2
+#define BEJA 3
+#define FARO 4
+#define SANTAREM 5
+#define GUARDA 6
+#define MADRID 7
+#define AVEIRO 8
+#define PORTO 9
+#define BRAGA 10
+#define CHAVES 11
+#define DILI 12
+
+
 /*Struct para guardar o ficheiro map*/
 struct map {
     int origem;
@@ -28,10 +48,10 @@ struct quest {
                 if ((lista[i].origem == origem && lista[i].destino == destino) || (lista[i].origem == destino && lista[i].destino == origem)) {
             count++;
                 }
-           
             }
             return count;
         }
+
         /*Função da task2*/
         int task02(struct map *lista, int n, int origem, int destino){
             int min = -1;
@@ -44,17 +64,18 @@ struct quest {
             }
             return min;
         }
+
         /*Função da task3*/
         int task03(struct map *lista, int n, int origem, int destino){
-            int preço = -1;
+            int preco = -1;
             for(int i = 0; i < n; i++){
                 if((lista[i].origem == origem && lista[i].destino == destino) || (lista[i].origem == destino && lista[i].destino == origem)){
-                    if (preço == -1 || lista[i].custo < preço ){
-                        preço = lista[i].custo;
+                    if (preco == -1 || lista[i].custo < preco ){
+                        preco = lista[i].custo;
                     }
                 }
             }
-            return preço;
+            return preco;
         }
         /*Função da task4*/
         int task04(struct map *lista, int n, int origem, int destino, int partida){
@@ -70,9 +91,37 @@ struct quest {
         }
     }
     return cedo;
-}
-        
 
+    /*Funcao da task5*/
+    int task05(struct map *lista, int n, int origem, int destino) {
+      int intermedias[20];            /*Lista de viagens que partem da origem*/
+      int n_intermedias = 0;
+
+    /* Procura todas as viagens que incluam a origem de que estamos a procura*/
+    for (int i = 0; i < n; i++) {
+        if (lista[i].origem == origem) {
+            intermedias[n_intermedias] = lista[i].destino;
+            n_intermedias++;
+        } else if (lista[i].destino == origem) {
+            intermedias[n_intermedias] = lista[i].origem;
+            n_intermedias++;
+        }
+    }
+  }
+}
+    /* Para cada cidade intermedia*/
+    for (int i = 0; i < n_intermedias; i++) {
+        
+        /*Procura se ha alguma ligacao da via intermedia com o destino de que estamos a procura*/
+        for (int j = 0; j < n; j++) {
+            if (lista[i].origem == intermedias[i] && lista[i].destino == destino ||
+                lista[i].destino == intermedias[i] && lista[i].origem == destino) {
+                return 1;
+            }
+        }
+    }
+
+    return -1;    
 
 
 int main () {
@@ -81,7 +130,7 @@ int main () {
     FILE *fptr; 
     fptr = fopen("Enunciado_mapa.map", "r");
 
-    /*ler o numero de cidades e ligações*/
+    /*ler o numero de cidades e ligacões*/
     int cidades, ligacoes;
     fscanf(fptr, "%d %d", &cidades, &ligacoes);
 
